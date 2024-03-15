787bf537bef85ca98cae98767dec3a3b56604d6a
NUTTER-11 <parulph7@gmail.com>
2024-03-15 21:38:00 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,18 @@ import cors from "cors";
 import "dotenv/config";
 import  mongoose  from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import MyRestaurantRoute from './routes/MyRestaurantRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 const app = express();
 
@@ -20,7 +29,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 
 app.use("/api/my/user", myUserRoute);
-
+app.use("/api/my/restaurant", MyRestaurantRoute);
 
 
 app.listen(7000, () => {
